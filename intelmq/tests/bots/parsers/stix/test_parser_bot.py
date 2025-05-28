38d323c4aed811e28d2418fd4d2741d4df2dd649
Ladislav Baco <lacike@users.noreply.github.com>
2025-05-29 00:04:37 +0200

    Improve STIX patterns parsing
    Use the official STIX2 Pattern Validator to get thecomparison expressions and extracts simple IoCs from them. Support for URLs, Domains, IPv4, IPv6 and also for MD5, SHA-1 and SHA-256 hashes. Small fixes and workarounds implemented to address certain anomalies in STIX data provided by some vendors (e.g. ETI) - SHA1 and SHA256 keywords accepted, invalid objects reported as Domains or URLs are dropped without throwing the exceptions

--- intelmq/tests/bots/parsers/stix/test_parser_bot.py
+++ intelmq/tests/bots/parsers/stix/test_parser_bot.py
@@ -40,6 +40,7 @@ EXAMPLE_EVENT = {'__type': 'Event',
                  }
 
 
+@test.skip_exotic()
 class TestStixParserBot(test.BotTestCase, unittest.TestCase):
     """
     A TestCase for a StixParserBot.
@@ -58,51 +59,93 @@ class TestStixParserBot(test.BotTestCase, unittest.TestCase):
 
     def test_pattern_url(self):
         """ Test if url pattern is parsed. """
-        indicator = self.bot_reference.parse_stix_pattern("[url:value = 'http://example.org']")
+        indicator = self.bot_reference.parse_stix_pattern("[url:value = 'http://example.org']")[0]
         self.assertEqual(str(indicator[0]), 'source.url')
         self.assertEqual(str(indicator[1]), 'http://example.org')
 
-    def test_pattern_url(self):
+    def test_pattern_domain(self):
         """ Test if domain pattern is parsed. """
-        indicator = self.bot_reference.parse_stix_pattern("[domain-name:value = 'example.org']")
+        indicator = self.bot_reference.parse_stix_pattern("[domain-name:value = 'example.org']")[0]
         self.assertEqual(str(indicator[0]), 'source.fqdn')
         self.assertEqual(str(indicator[1]), 'example.org')
 
     def test_pattern_ipv4(self):
         """ Test if ipv4 pattern is parsed. """
-        indicator = self.bot_reference.parse_stix_pattern("[ipv4-addr:value = '127.0.0.1']")
+        indicator = self.bot_reference.parse_stix_pattern("[ipv4-addr:value = '127.0.0.1']")[0]
         self.assertEqual(str(indicator[0]), 'source.ip')
         self.assertEqual(str(indicator[1]), '127.0.0.1')
 
     def test_pattern_ipv4_cidr(self):
         """ Test if ipv4 cidr pattern is parsed. """
-        indicator = self.bot_reference.parse_stix_pattern("[ipv4-addr:value = '127.0.0.0/8']")
+        indicator = self.bot_reference.parse_stix_pattern("[ipv4-addr:value = '127.0.0.0/8']")[0]
         self.assertEqual(str(indicator[0]), 'source.network')
         self.assertEqual(str(indicator[1]), '127.0.0.0/8')
 
     def test_pattern_ipv4_cidr_single_host(self):
         """ Test if ipv4 cidr with single host pattern is parsed. """
-        indicator = self.bot_reference.parse_stix_pattern("[ipv4-addr:value = '127.0.0.1/32']")
+        indicator = self.bot_reference.parse_stix_pattern("[ipv4-addr:value = '127.0.0.1/32']")[0]
         self.assertEqual(str(indicator[0]), 'source.ip')
         self.assertEqual(str(indicator[1]), '127.0.0.1')
 
     def test_pattern_ipv6(self):
         """ Test if ipv6 pattern is parsed. """
-        indicator = self.bot_reference.parse_stix_pattern("[ipv6-addr:value = '::1']")
+        indicator = self.bot_reference.parse_stix_pattern("[ipv6-addr:value = '::1']")[0]
         self.assertEqual(str(indicator[0]), 'source.ip')
         self.assertEqual(str(indicator[1]), '::1')
 
     def test_pattern_ipv6_cidr(self):
         """ Test if ipv6 cidr pattern is parsed. """
-        indicator = self.bot_reference.parse_stix_pattern("[ipv6-addr:value = 'fe:80::/10']")
+        indicator = self.bot_reference.parse_stix_pattern("[ipv6-addr:value = 'fe:80::/10']")[0]
         self.assertEqual(str(indicator[0]), 'source.network')
         self.assertEqual(str(indicator[1]), 'fe:80::/10')
 
     def test_pattern_ipv6_cidr_single_host(self):
         """ Test if ipv6 cidr with single host pattern is parsed. """
-        indicator = self.bot_reference.parse_stix_pattern("[ipv6-addr:value = 'fe:80::1/128']")
+        indicator = self.bot_reference.parse_stix_pattern("[ipv6-addr:value = 'fe:80::1/128']")[0]
         self.assertEqual(str(indicator[0]), 'source.ip')
         self.assertEqual(str(indicator[1]), 'fe:80::1')
+
+    def test_pattern_hash_md5(self):
+        """ Test if domain pattern is parsed. """
+        indicator = self.bot_reference.parse_stix_pattern("[file:hashes.MD5 = '44d88612fea8a8f36de82e1278abb02f']")[0]
+        self.assertEqual(str(indicator[0]), 'malware.hash.md5')
+        self.assertEqual(str(indicator[1]), '44d88612fea8a8f36de82e1278abb02f')
+
+    def test_pattern_hash_sha1(self):
+        """ Test if domain pattern is parsed. """
+        indicator = self.bot_reference.parse_stix_pattern("[file:hashes.'SHA-1' = '3395856ce81f2b7382dee72602f798b642f14140']")[0]
+        self.assertEqual(str(indicator[0]), 'malware.hash.sha1')
+        self.assertEqual(str(indicator[1]), '3395856ce81f2b7382dee72602f798b642f14140')
+
+        # Based on 10.7 Hashing Algorithm Vocabulary, keys SHA1 and SHA256 are not valid, but they are used in some feeds (e.g. ETI)
+        # https://docs.oasis-open.org/cti/stix/v2.1/os/stix-v2.1-os.html#_ths0b11wzxv3
+        indicator = self.bot_reference.parse_stix_pattern("[file:hashes.SHA1 = '3395856ce81f2b7382dee72602f798b642f14140']")[0]
+        self.assertEqual(str(indicator[0]), 'malware.hash.sha1')
+        self.assertEqual(str(indicator[1]), '3395856ce81f2b7382dee72602f798b642f14140')
+
+    def test_pattern_hash_sha256(self):
+        """ Test if domain pattern is parsed. """
+        indicator = self.bot_reference.parse_stix_pattern("[file:hashes.'SHA-256' = '275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f']")[0]
+        self.assertEqual(str(indicator[0]), 'malware.hash.sha256')
+        self.assertEqual(str(indicator[1]), '275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f')
+
+        # Based on 10.7 Hashing Algorithm Vocabulary, keys SHA1 and SHA256 are not valid, but they are used in some feeds (e.g. ETI)
+        # https://docs.oasis-open.org/cti/stix/v2.1/os/stix-v2.1-os.html#_ths0b11wzxv3
+        indicator = self.bot_reference.parse_stix_pattern("[file:hashes.SHA256 = '275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f']")[0]
+        self.assertEqual(str(indicator[0]), 'malware.hash.sha256')
+        self.assertEqual(str(indicator[1]), '275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f')
+
+    def test_complex_pattern1(self):
+        """ Test if complex pattern is parsed. """
+        indicators = self.bot_reference.parse_stix_pattern("[url:value = 'http://example.org' AND ipv4-addr:value = '127.0.0.1/32']")
+        self.assertEqual(('source.url', 'http://example.org') in indicators, True)
+        self.assertEqual(('source.ip', '127.0.0.1') in indicators, True)
+
+    def test_complex_pattern2(self):
+        """ Test if complex pattern is parsed. """
+        indicators = self.bot_reference.parse_stix_pattern("[url:value = 'http://example.org'] AND [ipv4-addr:value = '127.0.0.1/32']")
+        self.assertEqual(('source.url', 'http://example.org') in indicators, True)
+        self.assertEqual(('source.ip', '127.0.0.1') in indicators, True)
 
 
 if __name__ == '__main__':  # pragma: no cover
